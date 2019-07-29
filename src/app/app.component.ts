36ec2cb7b2479a6e29b38ae174a976daaa731035
Stefan Jaindl <jaindl.stefan@gmail.com>
2019-07-30 00:48:10 +0200

    feat: Landing Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TravelCompanionHomepage';
+  title = 'The Travel Companion'
+  subtitle = 'A travel app that offers all in one place: Discovering, planning and remembering travels.'
+  videoLink = 'https://youtu.be/IEebsEjLKN8&autoplay=1'
 }
