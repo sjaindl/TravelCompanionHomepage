697d3a9a036ce4e3bbf17a6de6d8d751448f56e8
Stefan Jaindl <jaindl.stefan@gmail.com>
2019-07-30 18:58:18 +0200

    Fix: Add missing BrowserAnimationsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
@@ -10,6 +11,7 @@ import { CookieLawModule } from 'angular2-cookie-law'
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     CookieLawModule
